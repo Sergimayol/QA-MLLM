6a64a8738ba2518a8bd196ba02bc7cc545a62801
Sergimayol <sergi2808@gmail.com>
2023-06-26 20:58:26 +0200

    code: Entry file for 'testing' the application.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
+"""Entry file for 'testing' the application.
+"""
 import torch
 from preprocessor.input_analysis import InputAnalyzer
 from models import model as AIModel
-from utils.config import MODELS
+from utils.config import MODELS, DEBUG
+from utils.helpers import Helpers
 
 FILE_PATH = "../file.test.txt"
 
 
 def main():
-    print("Hello World!")
-    input_analyzer = InputAnalyzer(FILE_PATH)
+    in_path, out_path, model_path, device = Helpers.get_app_args()
+    if DEBUG:
+        in_path = FILE_PATH
+
+    input_analyzer = InputAnalyzer(in_path)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # print(input_analyzer.analyze())
+
     model = AIModel.QAModel(
         context=input_analyzer.analyze(),
         device=device,
@@ -19,7 +26,6 @@ def main():
         pipeline_name="question-answering",
     )
     print(model)
-
     print(model.answer_question("What person works for an IT company?"))
 
     model = AIModel.SummarizationModel(
@@ -31,7 +37,6 @@ def main():
         framework_name=MODELS["summarization"]["framework_name"],
     )
     print(model)
-
     print(model.summarize(300))
 
 
